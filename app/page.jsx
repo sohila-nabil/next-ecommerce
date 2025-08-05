196dc757c23aaefafe869c9bf1be5971b49fec4a
sohila-nabil <nsohila03@gmail.com>
2025-08-05 10:06:45 +0300

    updated

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import React from "react";
 import HeaderSlider from "@/components/HeaderSlider";
 import HomeProducts from "@/components/HomeProducts";
@@ -9,9 +9,10 @@ import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
 const Home = () => {
+
   return (
     <>
-      <Navbar/>
+      <Navbar />
       <div className="px-6 md:px-16 lg:px-32">
         <HeaderSlider />
         <HomeProducts />
